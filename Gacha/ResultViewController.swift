dc9d6f6efbb1daa3c9d58ca4710c89b0302a9e95
arisa isshiki <alisa19970113@gmail.com>
2018-02-05 17:07:22 +0900

    aaa

--- Gacha/ResultViewController.swift
+++ Gacha/ResultViewController.swift
@@ -61,6 +61,8 @@ class ResultViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+     
+     //aaaa
     }
     */
 
